b9e8caa6c1053de0753e99a285f1b8a8fde208d5
Lucas Silva <dev.lucassilva@gmail.com>
2023-02-13 15:50:40 -0300

    metodo para deletar inscriçao de assinatura

--- src/Assets/Subscriptions.php
+++ src/Assets/Subscriptions.php
@@ -23,4 +23,14 @@
             $response = $this->request('PATCH', $endpoint, $data);
             return json_decode($response->getBody()->getContents());
         }
+
+        public function unsubscribe($subscription_id) {
+            $endpoint = 'https://api.pagar.me/core/v5/subscriptions/'.$subscription_id.'';
+
+            $response = $this->request('DELETE', $endpoint, [
+                'cancel_pending_invoices' => true
+            ]);
+            
+            return json_decode($response->getBody()->getContents());
+        }
     }
